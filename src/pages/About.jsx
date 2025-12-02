fe0fa6d17b84ff49fb32d4ff921e0c1dfd81ff6c
Augastin29 <161020212+Augastin29@users.noreply.github.com>
2025-12-02 06:00:28 +0530

    add all file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,29 @@
+import{useEffect,useState} from 'react';
+
 export const About = () => {
+     const userbasicdata={name:'samaug',age:28,from:'tirunelveli'};
+    const [userval,setUserval]=useState(userbasicdata);
     const data=[{id:1,
         question:'who is this',
          answer:'react have multi option but main of the function reusable component'},
         {id:2,
         question:'where find data strure',
          answer:' date find the json and object and array'}
-       ,]
+       ,];
+
+      
+
+
+  useEffect(()=>{setUserval(userbasicdata)},[]);
     return(
         <>
         <div style={{textAlign:"center" ,backgroundColor:'#F2F2F2'}}>{data.map((item)=>{
            return <p key={item.id}>{item.question}</p>
-        })}</div>
+        })}
+        <p>{userval.name}</p>
+        </div>
+        
+        
         </>
     )
 }
