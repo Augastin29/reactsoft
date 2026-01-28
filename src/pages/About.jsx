6721916d0fc1c5be7b78991d775ab05442dd6a8d
Augastin29 <161020212+Augastin29@users.noreply.github.com>
2026-01-28 06:32:45 +0530

    add the correct word to the spelling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,29 +1,49 @@
-import{useEffect,useState} from 'react';
-
+import { useEffect, useState } from 'react';
+import "./About.css"
 export const About = () => {
-     const userbasicdata={name:'samaug',age:28,from:'tirunelveli'};
-    const [userval,setUserval]=useState(userbasicdata);
-    const data=[{id:1,
-        question:'who is this',
-         answer:'react have multi option but main of the function reusable component'},
-        {id:2,
-        question:'where find data strure',
-         answer:' date find the json and object and array'}
-       ,];
+  const userbasicdata = { name: 'samaug', age: 28, from: 'tirunelveli' };
+  const [userval, setUserval] = useState(userbasicdata);
+  const data = [{
+    id: 1,
+    question: 'who is this',
+    answer: 'react have multi option but main of the function reusable component'
+  },
+  {
+    id: 2,
+    question: 'where find data strure',
+    answer: ' date find the json and object and array'
+  }
+    ,];
 
-      
 
 
-  useEffect(()=>{setUserval(userbasicdata)},[]);
-    return(
-        <>
-        <div style={{textAlign:"center" ,backgroundColor:'#F2F2F2'}}>{data.map((item)=>{
-           return <p key={item.id}>{item.question}</p>
-        })}
+
+  useEffect(() => { setUserval(userbasicdata) }, []);
+  return (
+    <>
+      <div style={{ textAlign: "center", backgroundColor: '#F2F2F2' }}>{data.map((item) => {
+        return <p key={item.id}>{item.question}</p>
+      })}
         <p>{userval.name}</p>
+      </div>
+      <div className="food-container">
+        <div className="fos" style={{width:"30%"}}>
+          <img src="http://localhost:5173/reactsoft/image/delicious-food-social-media-template/28nov_7.jpg" alt="Food 1" />
+          {/* <h4>Biryani</h4> */}
         </div>
-        
-        
-        </>
-    )
+
+        <div className="food-card">
+          <img src="http://localhost:5173/reactsoft/image/delicious-food-social-media-template/28nov_7.jpg" alt="Food 2" />
+          <h4>Pizza</h4>
+        </div>
+
+        <div className="food-card">
+          <img src="http://localhost:5173/reactsoft/image/samosa-food-social-media-promotion-instagram-banner-post-template-design\samosa.jpg" alt="Food 3" />
+          <h4>Burgers</h4>
+        </div>
+      </div>
+
+
+    </>
+  )
 }
